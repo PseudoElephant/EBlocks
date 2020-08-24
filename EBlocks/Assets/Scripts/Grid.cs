667ea37036cc08c2ec15b77c80972b80c4551a6e
Stephan Guingor <stephan.guingor04@gmail.com>
2020-08-24 17:54:30 -0500

    Grid

--- EBlocks/Assets/Scripts/Grid.cs
+++ EBlocks/Assets/Scripts/Grid.cs
@@ -1,27 +1,7 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Grid : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-=======
 ﻿using UnityEngine;
 using UnityEditor;
 using System.IO;
 
-[RequireComponent(typeof(MeshFilter))]
 public class Grid : MonoBehaviour
 {
 
@@ -30,61 +10,40 @@ public class Grid : MonoBehaviour
     public int widthBoard;
     public int heightBoard;
 
-    public GameObject background;
+ 
+    public GameObject linePrefabUp;
+    public GameObject linePrefabHor;
+
 
     public GameObject blockPrefab;
 
 
-    public TextAsset textAsset;
-    
- [MenuItem("Tools/Write file")]
-    static void WriteString()
-    {
-        string path = "Assets/Resources/test.txt";
-
-        //Write some text to the test.txt file
-        StreamWriter writer = new StreamWriter(path, true);
-        writer.WriteLine("Test");
-        writer.Close();
-
-        //Re-import the file to update the reference in the editor
-        AssetDatabase.ImportAsset(path); 
-        TextAsset asset = (TextAsset) Resources.Load("test");
-
-        //Print the text from the file
-        Debug.Log(asset.text);
-    }
-
-    [MenuItem("Tools/Read file")]
-    static void ReadString()
-    {
-        string path = "Assets/Resources/test.txt";
-
-        //Read the text from directly from the test.txt file
-        StreamReader reader = new StreamReader(path); 
-        Debug.Log(reader.ReadToEnd());
-        reader.Close();
-    }
     // Start is called before the first frame update
     void Start()
     {
     
-    Camera mainCamera = Camera.main;
-    float sizeOfProjection = (0.5466F*Mathf.Max(widthBoard,heightBoard)) + 1.7007F; // Regression line gathered from data y = 0.5466x + 1.7007
+        Camera mainCamera = Camera.main;
+        float sizeOfProjection = (0.5466F*Mathf.Max(widthBoard,heightBoard)) + 1.7007F; // Regression line gathered from data y = 0.5466x + 1.7007
 
-        mainCamera.orthographicSize = sizeOfProjection;
-     
-        mainCamera.transform.position = new Vector3(widthBoard/2, heightBoard/2, -10);
+            mainCamera.orthographicSize = sizeOfProjection;
         
-    
+            mainCamera.transform.position = new Vector3(widthBoard/2, heightBoard/2, -10);
+            
+        
 
-    // Intantiate Grid Could be built from a layout
+        // Intantiate Grid Could be built from a layout
+        for(int i = 1;i<widthBoard;i++){
+            GameObject lRendererRef= Instantiate(linePrefabUp,new Vector3(i,0,1),Quaternion.identity);
+            lRendererRef.transform.localScale = new Vector3(1,heightBoard,1);
+      
 
-    for(int i = 0;i<widthBoard;i++){
-        for(int j = 0;j<heightBoard;j++){
-            Instantiate(blockPrefab, new Vector3(i, j, 0), Quaternion.identity);
+
+            
+            for(int j = 1;j<heightBoard;j++){
+                GameObject lRendererRef2 = Instantiate(linePrefabHor,new Vector3(0,j,1),Quaternion.identity);
+            lRendererRef2.transform.localScale = new Vector3(widthBoard,1,1);
+            }
         }
-    }
     }
 
     
@@ -94,6 +53,5 @@ public class Grid : MonoBehaviour
 
     }
 
-    
->>>>>>> 64eee906655a14488020621cbc617546d808ffcd
+
 }
