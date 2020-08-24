64eee906655a14488020621cbc617546d808ffcd
Stephan Guingor <stephan.guingor04@gmail.com>
2020-08-24 16:17:35 -0500

    Message

---
+++ EBlocks/Assets/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    SpriteRenderer spriteRenderer;
+    public Sprite sprite;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver() {
+         if (Input.GetMouseButton(0)){
+            spriteRenderer.sprite = sprite;
+         }
+      
+    }
+}
---
+++ EBlocks/Assets/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+public  float stepDuration = 0.25f;
+public bool hopMove = false;
+private Coroutine playerMovement;
+
+private delegate IEnumerator Move(Vector3 direction);
+ Move moveMethod;
+private void Start() {
+    if (hopMove)
+        moveMethod = HopMove;
+    else
+        moveMethod = MovePlayer;
+}
+private void Update()
+{
+    if (playerMovement == null)
+    {
+        if (Input.GetKey(KeyCode.W))        //In general not a good idea to use Input.GetKey; use Input.GetButton instead
+            playerMovement = StartCoroutine(moveMethod(Vector3.up));
+        else if (Input.GetKey(KeyCode.S))
+            playerMovement = StartCoroutine(moveMethod(Vector3.down));
+        else if (Input.GetKey(KeyCode.D))
+            playerMovement = StartCoroutine(moveMethod(Vector3.right));
+        else if (Input.GetKey(KeyCode.A))
+            playerMovement = StartCoroutine(moveMethod(Vector3.left));
+    }
+}
+    private IEnumerator MovePlayer(Vector3 direction)
+{
+    Vector2 startPosition = transform.position;
+    Vector2 destinationPosition = transform.position + direction;
+    float t = 0.0f;
+
+    while (t < 1.0f)
+    {
+        transform.position = Vector2.Lerp(startPosition, destinationPosition, t);
+        t += Time.deltaTime / stepDuration;
+        yield return new WaitForEndOfFrame();
+    }
+
+    transform.position = destinationPosition;
+
+    playerMovement = null;
+}
+
+    private IEnumerator HopMove(Vector3 direction)
+{
+ 
+        transform.position += direction; 
+        
+        yield return new WaitForSeconds(stepDuration);
+    
+    playerMovement = null;
+}
+
+ 
+}
